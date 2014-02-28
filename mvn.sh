e4a1c53b10504714ca131a5eb43623bf74a9fa52
Jimmy Hardison <jchardis2@gmail.com>
2014-02-27 19:54:19 -0500

    a

--- mvn.sh
+++ mvn.sh
@@ -1,10 +1,12 @@
+cd /home/jchardis/git/InfinityAppSolutions/infinityappsolutions.lib
+mvn clean install
 cd /home/jchardis/git/InfinityAppSolutions/infinityappsolutions.server.lib
 mvn clean install
 cd /home/jchardis/git/InfinityAppSolutions/InfinityAppSolutions
 mvn clean install
 cd /home/jchardis/git/InfinityAppSolutions/WebDesigner
 mvn clean install
-cd /home/jchardis/git/InfinityAppSolutions/Terms-lib
+cd /home/jchardis/git/InfinityAppSolutions/infinityappsolutions.webterms.lib
 mvn clean install
 cd /home/jchardis/git/InfinityAppSolutions/WebTerms
 mvn clean install
--- mvnwebterms.sh
+++ mvnwebterms.sh
@@ -1,6 +1,8 @@
+cd /home/jchardis/git/InfinityAppSolutions/infinityappsolutions.lib
+mvn clean install
 cd /home/jchardis/git/InfinityAppSolutions/infinityappsolutions.server.lib
 mvn clean install
-cd /home/jchardis/git/InfinityAppSolutions/Terms-lib
+cd /home/jchardis/git/InfinityAppSolutions/infinityappsolutions.webterms.lib
 mvn clean install
 cd /home/jchardis/git/InfinityAppSolutions/WebTerms
 mvn clean install
