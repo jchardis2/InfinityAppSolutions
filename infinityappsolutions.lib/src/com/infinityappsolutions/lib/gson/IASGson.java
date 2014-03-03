df0c14a82320386271d5957412f8dacdfceed038
Jimmy Hardison <jchardis2@gmail.com>
2014-03-03 01:32:51 -0500

    a

--- infinityappsolutions.lib/src/com/infinityappsolutions/lib/gson/IASGson.java
+++ infinityappsolutions.lib/src/com/infinityappsolutions/lib/gson/IASGson.java
@@ -1,11 +1,11 @@
 package com.infinityappsolutions.lib.gson;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonReader;
 
 public class IASGson<T> {
 
@@ -26,4 +26,21 @@ public class IASGson<T> {
 		return gson.fromJson(gsonString, listOfTestObject);
 	}
 
+	public T fromGson(String gsonString, Class<T> classT)
+			throws JsonSyntaxException {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.fromJson(gsonString, classT);
+	}
+
+	public T fromGsonReader(JsonReader jsonReader, Type listOfTestObject)
+			throws JsonSyntaxException {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.fromJson(jsonReader, listOfTestObject);
+	}
+
+	public T fromGsonReader(JsonReader jsonReader, Class<T> classT)
+			throws JsonSyntaxException {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.fromJson(jsonReader, classT);
+	}
 }
