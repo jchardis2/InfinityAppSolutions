595429a0a25861652b19a49a65ada1a8f92ce4b5
Jimmy Hardison <jchardis2@gmail.com>
2014-03-19 18:53:14 -0400

    Revised repository collection

--- infinityappsolutions.server.lib/src/com/infinityappsolutions/server/lib/resource/AbstractWebAppResource.java
+++
@@ -1,5 +0,0 @@
-package com.infinityappsolutions.server.lib.resource;
-
-public abstract class AbstractWebAppResource implements IWebAppResource {
-	
-}
--- infinityappsolutions.server.lib/src/com/infinityappsolutions/server/lib/resource/IWebAppResource.java
+++
@@ -1,5 +0,0 @@
-package com.infinityappsolutions.server.lib.resource;
-
-public interface IWebAppResource {
-	
-}
